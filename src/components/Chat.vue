6ef0b4955f2c8bcb60f36e3b6bd8d8c407a99c36
Camilo Villota Ibarra <caviva@hotmail.com>
2024-08-30 18:48:34 -0500

    Improvements GUI

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,24 +1,55 @@
 <template>
   <div class="chat-container">
     <div ref="messageContainer" class="messages">
-      <div v-for="(message, index) in messages" :key="index" class="message">
-        <strong>{{ message.sender }}:</strong> {{ message.text }}
+      <div
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="['message', message.sender === 'Yo' ? 'user-message' : 'bot-message']"
+      >
+        <img
+          v-if="message.sender === 'Mia'"
+          src="https://avatar.iran.liara.run/public/85"
+          alt="Mia Avatar"
+          class="avatar"
+        />
+        <img
+          v-else
+          src="https://avatar.iran.liara.run/public/44"
+          alt="User Avatar"
+          class="avatar"
+        />
+        <div class="message-content">
+          <strong>{{ message.sender }}:</strong> {{ message.text }}
+        </div>
+      </div>
+      <div v-if="isTyping" class="typing-indicator">
+        <img
+          src="https://avatar.iran.liara.run/public/85"
+          alt="Mia Avatar"
+          class="avatar"
+        />
+        <div class="dots">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
     <div class="input-area">
       <input
         v-model="userInput"
-        @keyup.enter="sendMessage"
+        @keyup.enter="debouncedSendMessage"
         :placeholder="isLoading ? 'Cargando...' : 'Escribe tu mensaje...'"
         :disabled="isLoading"
       />
-      <button @click="sendMessage" :disabled="isLoading">Enviar</button>
+      <button @click="debouncedSendMessage" :disabled="isLoading">Enviar</button>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import debounce from "lodash.debounce";
 
 export default {
   data() {
@@ -26,56 +57,53 @@ export default {
       userInput: "",
       messages: [],
       conversationHistory: [],
-      isLoading: true, // Estado de carga
+      isLoading: true,
+      isTyping: false,
     };
   },
   async created() {
-    try {
-      // Cargar el contenido del prompt desde el archivo .txt en el directorio public
-      const response = await fetch("/system-prompt.txt");
-      if (!response.ok) {
-        throw new Error("Error al cargar el archivo del prompt del sistema.");
-      }
-      const systemPrompt = await response.text();
-
-      // Agregar el prompt del sistema al historial de la conversación
-      this.conversationHistory.push({
-        role: "system",
-        content: systemPrompt,
-      });
-
-      // Mia inicia la conversación
-      const initialMessage =
-        "Hola, soy Mia. La Maestra en inteligencia artificial creada por NuHo de Caracol Televisión y LUMO Media Lab. ¿Estás interesado en conocer sobre el proceso ideación de CreActivos? o ¿quieres preguntarme sobre otros temas de inteligencia artificial? ";
-      this.messages.push({
-        sender: "Mia",
-        text: initialMessage,
-      });
-
-      // Agregar el mensaje de Mia al historial de conversación
-      this.conversationHistory.push({
-        role: "assistant",
-        content: initialMessage,
-      });
-
-      this.isLoading = false; // Desactivar el estado de carga
-
-      // Desplazarse hacia abajo después de que Mia envíe el primer mensaje
-      this.scrollToBottom();
-    } catch (error) {
-      console.error("Error cargando el prompt del sistema:", error);
-      this.messages.push({
-        sender: "Mia",
-        text:
-          "No pude cargar la información inicial. Por favor, intenta recargar la página.",
-      });
-      this.isLoading = false;
-    }
+    await this.loadSystemPrompt();
+    this.isLoading = false;
   },
   updated() {
-    this.scrollToBottom(); // Desplazar hacia abajo cuando se actualiza el componente
+    this.scrollToBottom();
   },
   methods: {
+    async loadSystemPrompt() {
+      try {
+        const response = await fetch("/system-prompt.txt");
+        if (!response.ok) {
+          throw new Error("Error al cargar el archivo del prompt del sistema.");
+        }
+        const systemPrompt = await response.text();
+
+        this.conversationHistory.push({
+          role: "system",
+          content: systemPrompt,
+        });
+
+        const initialMessage =
+          "Hola, soy Mia. La Maestra en inteligencia artificial creada por NuHo de Caracol Televisión y LUMO Media Lab. ¿Estás interesado en conocer sobre el proceso ideación de CreActivos? o ¿quieres preguntarme sobre otros temas de inteligencia artificial? ";
+        this.messages.push({
+          sender: "Mia",
+          text: initialMessage,
+        });
+
+        this.conversationHistory.push({
+          role: "assistant",
+          content: initialMessage,
+        });
+
+        this.scrollToBottom();
+      } catch (error) {
+        console.error("Error cargando el cerebro de Mia: ", error);
+        this.messages.push({
+          sender: "Mia",
+          text:
+            "No pude cargar el cerebro de Mia. Por favor, intenta recargar la página.",
+        });
+      }
+    },
     scrollToBottom() {
       const container = this.$refs.messageContainer;
       container.scrollTop = container.scrollHeight;
@@ -85,7 +113,6 @@ export default {
         return;
       }
 
-      // Añadir el mensaje del usuario al chat
       const userMessage = {
         role: "user",
         content: this.userInput,
@@ -97,9 +124,9 @@ export default {
       this.conversationHistory.push(userMessage);
       this.userInput = "";
       this.isLoading = true;
+      this.isTyping = true;
 
       try {
-        // Llamar a la API de OpenAI con el formato para chat completions
         const completion = await axios.post(
           "https://api.openai.com/v1/chat/completions",
           {
@@ -114,14 +141,12 @@ export default {
           }
         );
 
-        // Añadir la respuesta del bot al chat
         const aiResponse = completion.data.choices[0].message.content;
         this.messages.push({
           sender: "Mia",
           text: aiResponse,
         });
 
-        // Actualizar el historial de la conversación con la respuesta del asistente
         this.conversationHistory.push({
           role: "assistant",
           content: aiResponse,
@@ -134,8 +159,12 @@ export default {
         });
       } finally {
         this.isLoading = false;
+        this.isTyping = false;
       }
     },
+    debouncedSendMessage: debounce(function () {
+      this.sendMessage();
+    }, 300),
   },
 };
 </script>
@@ -144,7 +173,7 @@ export default {
 .chat-container {
   display: flex;
   flex-direction: column;
-  height: 80vh; /* Ocupa toda la altura de la pantalla */
+  height: 80vh;
   max-width: 600px;
   margin: 0 auto;
   padding: 20px;
@@ -154,15 +183,85 @@ export default {
 }
 
 .messages {
-  flex: 1; /* Toma todo el espacio disponible */
+  flex: 1;
   overflow-y: auto;
   margin-bottom: 20px;
-  padding-right: 10px; /* Añadir padding para scroll */
+  padding-right: 10px;
+  transition: all 0.3s ease;
 }
 
 .message {
+  display: flex;
+  align-items: flex-start;
   margin-bottom: 10px;
   word-wrap: break-word;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
+  max-width: 70%;
+}
+
+.bot-message {
+  background-color: #e0e0e0;
+  text-align: left;
+  margin-right: auto;
+  border-bottom-left-radius: 0;
+  opacity: 0;
+  animation: fadeIn 0.5s forwards;
+}
+
+.user-message {
+  background-color: #007bff;
+  color: white;
+  text-align: right;
+  margin-left: auto;
+  border-bottom-right-radius: 0;
+  opacity: 0;
+  animation: fadeIn 0.5s forwards;
+}
+
+.message-content {
+  max-width: 100%;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+.typing-indicator {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+}
+
+.dots {
+  display: flex;
+  justify-content: space-between;
+  width: 20px;
+}
+
+.dots div {
+  width: 6px;
+  height: 6px;
+  background-color: #555;
+  border-radius: 50%;
+  animation: blink 1s infinite;
+}
+
+@keyframes blink {
+  50% {
+    opacity: 0;
+  }
+}
+
+@keyframes fadeIn {
+  to {
+    opacity: 1;
+  }
 }
 
 .input-area {
@@ -185,10 +284,12 @@ button {
   background-color: #007bff;
   color: white;
   cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.3s ease;
 }
 
 button:hover {
   background-color: #0056b3;
+  transform: scale(1.05);
 }
 
 button:disabled {
